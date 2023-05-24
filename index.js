8443a5aea04933efe5190f4c1919aa2c9ae89914
durgap09 <90268800+durgap09@users.noreply.github.com>
2023-05-24 15:02:54 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const DepGraph = require("dependency-graph").DepGraph;
-const pulumi = require("@pulumi/pulumi");
-const path = require("path");
-const node_modules_path = path.join(__dirname, "node_modules");
-const pulumiProgram = require("./pulumiProgram");
-const canvasJson = require("./canvas.json");
-pulumiProgram(canvasJson, DepGraph, pulumi, node_modules_path)();
-
